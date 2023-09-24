ca7399581786f393b728a0c67bead1df0118e30c
JAY KISHOR THAKUR <jay.21gcebaids026@galgotiacollege.edu>
2023-09-24 18:39:59 +0530

    Update jaykishorthakur1(largest element in an array).cpp

--- jaykishorthakur1(largest element in an array).cpp
+++ jaykishorthakur1(largest element in an array).cpp
@@ -23,38 +23,7 @@ int main(){
 }
 
 
-//second largest element in an array
  
-
-#include<iostream>
-#include <climits>
-using namespace std;
-
-int largestNumber(int arr[], int n){
-     int max= INT_MIN;
-     int secondmax= INT_MIN;
-     for(int i=0; i<n; i++){
-        if(arr[i]>max){
-            secondmax=max;
-            max= arr[i];
-        }
-        else{
-            if(arr[i]>secondmax and arr[i]<max){
-                secondmax= arr[i];
-            }
-        }
-     }
-     return secondmax;
-}
-
-int main(){
-    int arr[]={10,667,2753,297,6666,2,7,4,5};
-    int size=9;
-    int ans= largestNumber(arr, size);
-    cout<<"largest number:-"<<ans<<endl;
-}
-
-
 //insorted array:-
 int isSorted(int n, vector<int> &arr) {
       for(int i=0; i<n-1; i++){
