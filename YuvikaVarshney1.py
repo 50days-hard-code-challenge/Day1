217c3db199dcef667ec09eb7b142befc8addd747
YuvikaVarshney <130446237+YuvikaVarshney@users.noreply.github.com>
2023-09-24 10:27:31 +0530

    Add files via upload

---
+++ YuvikaVarshney1.py
@@ -0,0 +1,16 @@
+a=(input())
+b=(ord)(a[0])
+if b<=90 and b>=65:
+    print("1")
+elif b>=97 and b<=122:
+    print("0")
+else:
+    print("-1")
+"""ch = input()
+
+if ch >= 'A' and ch <= 'Z' :
+    print(1)
+elif ch >= 'a' and ch <= 'z' :
+    print(0)
+else :
+    print(-1)"""
