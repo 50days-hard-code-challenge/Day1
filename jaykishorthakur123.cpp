9fc0ad151c2ae1c1f82a52ce897163b4883f1419
JAY KISHOR THAKUR <jay.21gcebaids026@galgotiacollege.edu>
2023-09-24 18:29:22 +0530

    Create jaykishorthakur123.cpp

---
+++ jaykishorthakur123.cpp
@@ -0,0 +1,68 @@
+// largest element in an array
+ 
+#include<iostream>
+using namespace std;
+
+int largestNumber(int arr[], int n){
+    int max=arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>max){
+            max=arr[i];
+
+        }
+
+    }
+    return max;
+}
+
+int main(){
+    int arr[]={10,667,2753,297,6666,2,7,4,5};
+    int size=9;
+    int ans= largestNumber(arr, size);
+    cout<<"largest number:-"<<ans<<endl;
+}
+
+
+//second largest element in an array
+ 
+
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int largestNumber(int arr[], int n){
+     int max= INT_MIN;
+     int secondmax= INT_MIN;
+     for(int i=0; i<n; i++){
+        if(arr[i]>max){
+            secondmax=max;
+            max= arr[i];
+        }
+        else{
+            if(arr[i]>secondmax and arr[i]<max){
+                secondmax= arr[i];
+            }
+        }
+     }
+     return secondmax;
+}
+
+int main(){
+    int arr[]={10,667,2753,297,6666,2,7,4,5};
+    int size=9;
+    int ans= largestNumber(arr, size);
+    cout<<"largest number:-"<<ans<<endl;
+}
+
+
+//insorted array:-
+int isSorted(int n, vector<int> &arr) {
+      for(int i=0; i<n-1; i++){
+          if(arr[i]>arr[i+1]){
+              return 0;
+          }
+           
+      }
+      return 1;
+}
+ 
